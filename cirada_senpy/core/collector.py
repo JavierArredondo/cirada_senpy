3e82771dc0e06c695cd9a553a14e855100418eb0
javier <javier.arredondo.c@usach.cl>
2021-11-25 22:15:58 -0300

    feat(core): logic to download individual queries

---
+++ cirada_senpy/core/collector.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import requests
+from typing import List
+
+
+class Collector:
+    def __init__(self, data: pd.DataFrame):
+        self.data = data
+        self.headers = {"Content-Type": "application/json"}
+        self.url = "http://cutouts.cirada.ca/download_fits_batch/"
+        pass
+
+    def create_payloads(self) -> List[dict]:
+        payloads = []
+        for index, source in self.data.groupby("source_name"):
+            source_payload = []
+            for j, row in source.iterrows():
+                payload = {
+                    "filepath": f"/tmp/{row['filename']}",
+                    "filename": row["filename"],
+                    "row": index,
+                    "RA": row["ra"],
+                    "DEC": row["dec"],
+                    "only_mosaic": False
+                }
+                source_payload.append(payload)
+            payloads.append({index: source_payload})
+        return payloads
+
+    def download(self):
+        payloads = self.create_payloads()
+        for payload in payloads:
+            response = requests.request("POST", self.url, json=payload, headers=self.headers)
+            output_name = list(payload.keys())[0]
+            with open(f"{output_name}.tgz", "wb") as f:
+                f.write(response.content)
+
+
+if __name__ == "__main__":
+    from query import Query
+    query = Query(20, 20, "Abel 1314")
+    data = query.submit()
+    collector = Collector(data)
+    collector.download()
---
+++ cirada_senpy/core/query.py
@@ -0,0 +1,70 @@
+import json
+import pandas as pd
+import requests
+
+
+class Query:
+    def __init__(self,
+                 x_size: int,
+                 y_size: int,
+                 source_name: str or None = None,
+                 ra: float or str or None = None,
+                 dec: float or str or None = None,
+                 coord_system: str = "Equatorial",
+                 thumbnail: bool = True):
+        self.source_name = source_name
+        self.ra = ra
+        self.dec = dec
+        self.x_size = x_size
+        self.y_size = y_size
+        self.coord_system = coord_system
+
+        self.thumbnail = thumbnail
+        self.boundary = "---011000010111000001101001"
+        self.url = "http://cutouts.cirada.ca/rmcutout/rm_get_cutout/"
+        self.headers = {"Content-Type": f"multipart/form-data; boundary={self.boundary}"}
+
+    def create_payload(self) -> str:
+        payload = f"-----011000010111000001101001\r\nContent-Disposition: form-data; "
+        form_data = "Content-Disposition: form-data; "
+        if self.x_size:
+            payload += f"name=\"x_size\"\r\n\r\n{self.x_size}\r\n--{self.boundary}\r\n{form_data}"
+        if self.y_size:
+            payload += f"name=\"y_size\"\r\n\r\n{self.y_size}\r\n--{self.boundary}\r\n{form_data}"
+        if self.source_name:
+            payload += f"name=\"source_name\"\r\n\r\n{self.source_name}\r\n--{self.boundary}\r\n{form_data}"
+        if self.coord_system:
+            payload += f"name=\"typeOfLoc\"\r\n\r\n{self.coord_system}\r\n--{self.boundary}\r\n{form_data}"
+        if self.ra and self.dec:
+            payload += f"name=\"location\"\r\n\r\n{self.ra} {self.dec}\r\n--{self.boundary}\r\n{form_data}"
+        return payload
+
+    def create_response(self, response: str) -> pd.DataFrame:
+        response = json.loads(response[57:-5])  # the string response has weird content like ?END?
+        response = response["result"]
+        cutouts_rows = []
+        for key, value in response.items():
+            main_data = {
+                "source_name": value["source_name"],
+                "ra": value["location"][0],
+                "dec": value["location"][1],
+                "radius": value["radius"],
+            }
+            for element in value['fits_data']:
+                if not self.thumbnail:
+                    del element["thumbnail"]
+                element.update(main_data)
+                cutouts_rows.append(element)
+        cutouts_rows = pd.DataFrame(cutouts_rows)
+        return cutouts_rows
+
+    def submit(self) -> pd.DataFrame:
+        payload = self.create_payload()
+        response = requests.request("POST", self.url, data=payload, headers=self.headers)
+        response = self.create_response(response.text)
+        return response
+
+
+if __name__ == "__main__":
+    query = Query(20, 20, "Abel 1314")
+    query.submit()
